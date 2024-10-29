1db7fbe28f5c8f4f10ddc0e28d70b0b96136aede
Enesoruc69 <orucenes935@gmail.com>
2024-10-29 12:12:36 +0300

    Post isteği yapan carp endpointi eklendi

--- rest_api/rest_api.py
+++ rest_api/rest_api.py
@@ -9,5 +9,15 @@ class IkiTopla(Resource):
         sonuc = 2 + 2
         return {'sonuc': sonuc}, 200
 
+class Carp(Resource):
+    def post(self):
+        veri = request.get_json()
+        sayilar = veri.get('sayilar', [])
+        sonuc = 1
+        for sayi in sayilar:
+            sonuc *= sayi
+        return {'sonuc': sonuc}, 200
 
+api.add_resource(IkiTopla, '/iki_topla')
+api.add_resource(Carp, '/carp')
 
