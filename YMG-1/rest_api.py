ad5c2b1778d9dae209eb190686ac240ec0a91804
Enesoruc69 <orucenes935@gmail.com>
2024-10-29 11:56:24 +0300

    GET isteği yapan endpoint eklendi

--- YMG-1/rest_api.py
+++ YMG-1/rest_api.py
@@ -3,3 +3,9 @@ from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
+
+class IkiTopla(Resource):
+    def get(self):
+        sonuc = 2 + 2
+        return {'sonuc': sonuc}, 200
+
